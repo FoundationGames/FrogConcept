aea7e77038fd85b5f80d4f5afa369673f52abbb1
FoundationGames <43485105+FoundationGames@users.noreply.github.com>
2021-10-18 22:06:51 -0700

    Add spawn egg, fix frog speed, add croaking animations and framework for play-once frog animations

--- src/main/java/foundationgames/frogconcept/FrogConcept.java
+++ src/main/java/foundationgames/frogconcept/FrogConcept.java
@@ -2,11 +2,18 @@ package foundationgames.frogconcept;
 
 import foundationgames.frogconcept.entity.FrogEntity;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SpawnEggItem;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import software.bernie.geckolib3.GeckoLib;
@@ -18,11 +25,33 @@ public class FrogConcept implements ModInitializer {
             FabricEntityTypeBuilder.<FrogEntity>create(SpawnGroup.CREATURE, FrogEntity::new).dimensions(EntityDimensions.fixed(0.75f, 0.5f)).build()
     );
 
+    public static final Item FROG_SPAWN_EGG = Registry.register(Registry.ITEM, id("frog_spawn_egg"),
+            new SpawnEggItem(FROG, 0xa66341, 0x96774c, new Item.Settings().group(ItemGroup.MISC))
+    );
+
     @Override
     public void onInitialize() {
         GeckoLib.initialize();
 
         FabricDefaultAttributeRegistry.register(FROG, FrogEntity.createAttributes());
+
+        ClientPlayNetworking.registerGlobalReceiver(id("frog_queue_anim"), (client, handler, buf, responseSender) -> {
+            if (client.world == null) return;
+            var entity = client.world.getEntityById(buf.readInt());
+            int anim = buf.readInt();
+            client.execute(() -> {
+                if (entity instanceof FrogEntity frog) {
+                    frog.pushQueuedAnimIndex(anim);
+                }
+            });
+        });
+    }
+
+    public static void queueFrogAnim(ServerPlayerEntity player, FrogEntity frog, int anim) {
+        var buf = PacketByteBufs.create();
+        buf.writeInt(frog.getId());
+        buf.writeInt(anim);
+        ServerPlayNetworking.send(player, id("frog_queue_anim"), buf);
     }
 
     public static Identifier id(String path) {
--- src/main/java/foundationgames/frogconcept/entity/FrogEntity.java
+++ src/main/java/foundationgames/frogconcept/entity/FrogEntity.java
@@ -2,10 +2,7 @@ package foundationgames.frogconcept.entity;
 
 import foundationgames.frogconcept.FrogConcept;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ai.goal.LookAroundGoal;
-import net.minecraft.entity.ai.goal.SwimGoal;
-import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
-import net.minecraft.entity.ai.goal.WanderAroundGoal;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.data.DataTracker;
@@ -16,9 +13,9 @@ import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 import org.jetbrains.annotations.Nullable;
@@ -30,17 +27,14 @@ import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 import java.util.function.Predicate;
 
 public class FrogEntity extends AnimalEntity implements IAnimatable {
     public static final Type TEMPERATE = Type.register(FrogConcept.id("temperate"), biome -> true);
 
     public static final TrackedData<Type> FROG_TYPE = DataTracker.registerData(FrogEntity.class, Type.DATA_HANDLER);
-    public static final TrackedData<Integer> ANIMATION = DataTracker.registerData(FrogEntity.class, TrackedDataHandlerRegistry.INTEGER);
+    public static final TrackedData<Integer> LOOPING_ANIMATION = DataTracker.registerData(FrogEntity.class, TrackedDataHandlerRegistry.INTEGER);
 
     public static final String[] ANIMS = {
             "animation.frog.idle",
@@ -48,7 +42,9 @@ public class FrogEntity extends AnimalEntity implements IAnimatable {
             "animation.frog.walk",
             "animation.frog.swim",
             "animation.frog.ascend",
-            "animation.frog.descend"
+            "animation.frog.descend",
+            "animation.frog.eat",
+            "animation.frog.chew"
     };
     public static final int ANIM_IDLE = 0;
     public static final int ANIM_CROAK = 1;
@@ -56,11 +52,15 @@ public class FrogEntity extends AnimalEntity implements IAnimatable {
     public static final int ANIM_SWIM = 3;
     public static final int ANIM_ASCEND = 4;
     public static final int ANIM_DESCEND = 5;
+    public static final int ANIM_EAT = 6;
+    public static final int ANIM_CHEW = 7;
 
     private final AnimationFactory animations = new AnimationFactory(this);
 
-    private int lastAnimationIndex = 0;
     private boolean updateAnimation = false;
+    private Deque<Integer> animationQueue = new ArrayDeque<>();
+
+    private boolean idle = true;
 
     public FrogEntity(World world) {
         this(FrogConcept.FROG, world);
@@ -73,38 +73,44 @@ public class FrogEntity extends AnimalEntity implements IAnimatable {
     @Override
     protected void mobTick() {
         super.mobTick();
-        this.animationTick();
+        if (!world.isClient()) {
+            this.animationTick();
+        }
     }
 
     protected void animationTick() {
         var vel = getVelocity();
         int anim = -1;
-        if (!this.isTouchingWater()) {
+        this.idle = true;
+        if (!this.isTouchingWater() && !this.isOnGround()) {
             if (vel.getY() > 0) {
                 anim = ANIM_ASCEND;
-            } else if (vel.getY() < 0 && !this.isOnGround()) {
+            } else if (vel.getY() < 0) {
                 anim = ANIM_DESCEND;
             }
+            this.idle = false;
         }
-        if (vel.getX() > 0 || vel.getZ() > 0 || this.moveControl.isMoving()) {
+        if (vel.getX() != 0 || vel.getZ() != 0 || this.moveControl.isMoving()) {
             if (this.isOnGround()) {
                 anim = ANIM_WALK;
             } else if (this.isTouchingWater()) {
                 anim = ANIM_SWIM;
             }
+            this.idle = false;
         } else {
             anim = ANIM_IDLE;
         }
         if (anim >= 0) {
-            setAnimIndex(anim);
+            setLoopAnimIndex(anim);
         }
     }
 
     @Override
     protected void initGoals() {
         this.goalSelector.add(0, new SwimGoal(this));
-        this.goalSelector.add(4, new WanderAroundGoal(this, 1, 42));
-        this.goalSelector.add(5, new LookAroundGoal(this));
+        this.goalSelector.add(1, new CroakGoal(this));
+        this.goalSelector.add(2, new WanderAroundGoal(this, 1, 50));
+        this.goalSelector.add(3, new LookAroundGoal(this));
     }
 
     public Type getFrogType() {
@@ -115,30 +121,40 @@ public class FrogEntity extends AnimalEntity implements IAnimatable {
         this.dataTracker.set(FROG_TYPE, type == null ? Type.defaultType() : type);
     }
 
-    public int getAnimIndex() {
-        return this.dataTracker.get(ANIMATION);
+    public int getLoopAnimIndex() {
+        return this.dataTracker.get(LOOPING_ANIMATION);
     }
 
-    public void setAnimIndex(int index) {
-        this.dataTracker.set(ANIMATION, index);
+    public void setLoopAnimIndex(int index) {
+        this.dataTracker.set(LOOPING_ANIMATION, index);
+    }
+
+    public void pushQueuedAnimIndex(int index) {
+        if (world.isClient()) {
+            this.animationQueue.push(index);
+            return;
+        }
+
+        world.getPlayers().forEach(player -> {
+            if (player.getPos().distanceTo(getPos()) < 96 && player instanceof ServerPlayerEntity sPlayer) {
+                FrogConcept.queueFrogAnim(sPlayer, this, index);
+            }
+        });
     }
 
     @Override
     protected void initDataTracker() {
         super.initDataTracker();
         this.dataTracker.startTracking(FROG_TYPE, Type.defaultType());
-        this.dataTracker.startTracking(ANIMATION, ANIM_IDLE);
+        this.dataTracker.startTracking(LOOPING_ANIMATION, ANIM_IDLE);
     }
 
     @Override
     public void onTrackedDataSet(TrackedData<?> data) {
         super.onTrackedDataSet(data);
 
-        if (data.equals(ANIMATION)) {
-            if (getAnimIndex() != lastAnimationIndex) {
-                updateAnimation = true;
-            }
-            lastAnimationIndex = getAnimIndex();
+        if (data.equals(LOOPING_ANIMATION) && world.isClient()) {
+            updateAnimation = true;
         }
     }
 
@@ -149,13 +165,29 @@ public class FrogEntity extends AnimalEntity implements IAnimatable {
     }
 
     public static DefaultAttributeContainer.Builder createAttributes() {
-        return MobEntity.createMobAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 7.0D).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.09);
+        return MobEntity.createMobAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 7.0D).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.075);
+    }
+
+    @Override
+    public int getBodyYawSpeed() {
+        return 1;
     }
 
     private PlayState animationRefresh(AnimationEvent<FrogEntity> event) {
-        if (updateAnimation) {
-            int index = Math.max(0, getAnimIndex());
+        if (animationQueue.size() > 0) {
+            var anims = new AnimationBuilder();
+            for (int i : animationQueue) {
+                i = Math.max(0, i);
+                if (i > ANIMS.length) i = 0;
+                anims.addAnimation(ANIMS[i]);
+            }
+            animationQueue.clear();
+            event.getController().setAnimation(anims);
+            event.getController().markNeedsReload();
+        } else if (updateAnimation) {
+            int index = Math.max(0, getLoopAnimIndex());
             if (index > ANIMS.length) index = 0;
+            updateAnimation = false;
             event.getController().setAnimation(new AnimationBuilder().addAnimation(ANIMS[index]));
         }
 
@@ -170,6 +202,24 @@ public class FrogEntity extends AnimalEntity implements IAnimatable {
     @Override
     public AnimationFactory getFactory() {
         return animations;
+    }
+
+    public static class CroakGoal extends Goal {
+        private final FrogEntity frog;
+
+        public CroakGoal(FrogEntity frog) {
+            this.frog = frog;
+        }
+
+        @Override
+        public boolean canStart() {
+            return frog.idle && frog.world.random.nextInt(69) == 0;
+        }
+
+        @Override
+        public void start() {
+            frog.pushQueuedAnimIndex(ANIM_CROAK);
+        }
     }
 
     public static class Type {
